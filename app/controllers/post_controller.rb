77c70def0bbbd6a332cab4d563a81168b04fbfb8
takara <kourakazuki@takara.local>
2018-12-17 20:37:10 +0900

    3章終了

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,19 @@
+class PostController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+    @id = params[:id]
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/post/index")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get "post/index" => "post#index"
+  get "post/new" => "post#new"
+  get 'post/:id' => 'post#show'
   get '/' => 'home#top'
   get 'about' => 'home#about'
+  post 'post/create' => 'post#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
